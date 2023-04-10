c7e073d715d65e655ea60154cfb1c662d72d21b2
HiddenUndefined <georgerudenko97@gmail.com>
2023-04-10 22:36:27 +0300

    feat: add jest to project

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2023 Yuriy R. | github: HiddenUndefined
+ *
+ * This file is part of the «Algorithms in JS» | github: algorithms-in-js.
+ *
+ * The Algorithms in JS is licensed under the MIT License.
+ * See the LICENSE file in the project root for more information.
+ */
